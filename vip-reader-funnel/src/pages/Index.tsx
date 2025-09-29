6373d8551c8969767ac646457627d61100de67f1
Pedro-ghub <pedroaugustovicente1244@gmail.com>
2025-09-29 11:56:23 -0300

    Add files via upload

---
+++ vip-reader-funnel/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+// Update this page (the content is just a fallback if you fail to update the page)
+
+import UpsellLanding from '@/components/UpsellLanding';
+
+const Index = () => {
+  return <UpsellLanding />;
+};
+
+export default Index;
